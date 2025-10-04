d23e1cd9b9903313fd13edfec097d386eed0bb31
jozef-kasprzycki <jozek.kasprzycki@gmail.com>
2025-10-04 17:15:40 +0200

    Very basic movable player

---
+++ scripts/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+@export var speed = 400.0
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	#look_at(get_global_mouse_position())
+	velocity = input_direction * speed
+	
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+	
